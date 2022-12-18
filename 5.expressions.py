cde6f5690bf78c90240ea7ff69f7154a5eb6c655
Tushar Shukla 12219829 <115138208+shuklatushar12219829@users.noreply.github.com>
2022-12-18 12:04:24 +0530

    Add files via upload

---
+++ 5.expressions.py
@@ -0,0 +1,24 @@
+a=10
+b=20
+print(a+b)
+print(a-b)
+print(a/b)
+print(a*b)
+print(a%b)
+
+a,b,c,d,e=10,20,30,40,50
+z=a+b+c+d+e
+print("sum of a",a," b",b," c","d","e","is",z)
+z=a*b*c*d*e
+print("product of a",a,"b",c,"c",d,"e","is",z)
+radius=4
+area=3.14*radius*radius
+print("area of circle is",area)
+p=10000
+r=12
+t=3
+SI=(p*r*t)/100
+amount=p+SI
+print("the intrest is",SI)
+
+print("the total amount is",amount)
---
+++ 7.membership operator.py
@@ -0,0 +1,10 @@
+# x="abcd"
+# print("a"in x)
+# print("h" in x)
+# print("a" not in x)
+# print("h" not in x)
+# x=input("enter the string:")
+# y=input("check str:")
+# print(y in x)
+l1=[1,2,3,4,5]
+print(10 in l1)

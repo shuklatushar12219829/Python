451b814e973ac708663252bfe9b11eb47cd675f5
Tushar Shukla 12219829 <115138208+shuklatushar12219829@users.noreply.github.com>
2022-12-21 22:16:29 +0530

    Add files via upload

---
+++ class list.py
@@ -0,0 +1,65 @@
+# # # # list1=[1,2,"one","h",4,7,9,33,5]
+# # # # print(list1)
+# # # # print(list1[2-4])
+# # # # print(list1[0:3:2])
+# # # # print(list1[3::2])
+# # # # list2=[10,23,4,5]
+# # # # print(list1+list2)
+# # # # print(2*list2)
+# # # # n=int(input("Enter a value:"))
+# # # # print(list1*n)
+# # # # # list3=[1,2,["A",34.23],4]
+# # # # # print(list2[2[0]])
+# # # # list1.append(48)
+# # # # print(list1)
+# # # l1=list()
+# # # l1.append(1)
+# # # print(l1)
+# # # l1.append(2)
+# # # print(l1)
+# # # l1.append(3)
+# # # print(l1)
+# # # set={3,3.0}
+# # # print(set)
+# # # s={23,5,44,46,33}
+# # # print(s)
+# # # test={}
+# # # print(type(test))
+# # a=set()
+# # # a.add(1)
+# # print(a)
+# # b=()
+# # print(b)
+# # print(type(b))
+# # b=(2,"tushar",21343,65,"tdsodjs")
+# # print(b)
+# # print(b[3])
+# # b[0]=5
+# # print(b)
+#
+# mydic={}
+# print(mydic)
+# print(type(mydic))
+# dic={"andra_pradesh":"amravati",
+#          "utter pradesh":"lucknow"}
+# print(dic["andra_pradesh"])
+#
+# mydic1={"laptop":80000,
+#         "mobile":20000}
+# mydic1["laptop"]=70000
+# print(mydic1["laptop"])
+# print(mydic1.get("laptop"))
+# dic2={"aaa":1,
+#       "bbb":2,
+#       "ddd":3}
+# print(dic2.get("ccc"))
+# print(dic2["ccc"])
+s="0011"
+print(int(s,2))
+print(int(s))
+w=int(input("enter a value:"),16)
+print(w)
+print(float("  23.45\n"))
+# print(float("hello python"))
+print(hex(2))
+

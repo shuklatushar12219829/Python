a67a09062cbc1690329fd94d67ef9b0a05cda8a9
Tushar Shukla 12219829 <115138208+shuklatushar12219829@users.noreply.github.com>
2022-12-26 01:35:29 +0530

    Add files via upload

---
+++ eval.py
@@ -0,0 +1,10 @@
+x,y=eval(input("Print two values:"))
+print(x,y)
+# import math
+# r,l=eval(input("enter radius and length:"))
+# volume=math.pi*r**2*l
+# print("volume:",volume)
+# print("2+3")
+# print(eval("2+3"))
+# print("3.4+5.6")
+# print(eval("3.4+5.6"))

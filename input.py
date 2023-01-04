914a5ef1b2faf3452d8f09a33e3ca1867b0f4aae
Tushar Shukla 12219829 <115138208+shuklatushar12219829@users.noreply.github.com>
2023-01-04 23:42:51 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,8 @@
+# a=input("enter string value:")
+# b=input("enter input value:")
+# c=input("enter input value:")
+# d=input("enter input value:")
+#
+# print(a+" "+b+" "+c+" "+d)
+a=input("enter your name:")
+print(a)
---
+++ interchange.py
@@ -0,0 +1,5 @@
+a,b=10,20
+a=a*b
+b=a//b
+a=a//b
+print(a,b)

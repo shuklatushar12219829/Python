93d5e69bef33e0804f4526bf24d776b641f213e4
Tushar Shukla 12219829 <115138208+shuklatushar12219829@users.noreply.github.com>
2023-01-06 20:28:58 +0530

    Add files via upload

---
+++ logical.py
@@ -0,0 +1,5 @@
+'''jee,twelth=eval(input("Enter the values"))
+print(jee>=85 and twelth>=60)
+print(jee>=85 or twelth>=60)'''
+age,membership=eval(input("enter age and type of membership:"))
+print(age>=60 and membership=="prime")
---
+++ practicesheet.py
@@ -0,0 +1,47 @@
+# '''a=int(input("enter the temperature in celsius-"))
+# f=(9/5)*a+32
+# k=273+a
+# print("temperature in fahrenheit-",f)
+# print("temperature in kelvin is-",k)'''
+#
+# '''radius=int(input("Enter the radius-"))
+# length=int(input("Enter the length-"))
+# volume=3.14*radius**2*length
+# print("the volume of cylinder is",volume)'''
+#
+# '''feet=float(input("Enter value in feet-"))
+# metre=feet*.305
+# print("value in metre-",metre)'''
+#
+# '''a=float(input("Enter the value-"))
+# gratuity=15*a/100
+# b=a+gratuity
+# print("the value of gratuity is-",gratuity)
+# print("the value of subtotal is-",b)'''
+#
+# '''x1=float(input("Enter the value-"))
+# x2=float(input("Enter the value-"))
+# y1=float(input("Enter the value-"))
+# y2=float(input("Enter the value-"))
+# distance=((x2-x1)**2+(y2-y1)**2)**.5
+# print("the distane is-",distance)'''
+#
+# '''M=float(input("Amount of water in kg-"))
+# intial_temp=float(input("Enter the initial temperature-"))
+# final_temp=float(input("Enter the final temperature-"))
+# Q=M*(final_temp-intial_temp)*4184
+# print("The Energy Needed is-",Q)'''
+#
+# x1=float(input("enter the value of x1:"))
+# y1=float(input("enter the value of y1:"))
+# x2=float(input("enter the value of x2:"))
+# y2=float(input("enter the value of y2:"))
+# x3=float(input("enter the value of x3:"))
+# y3=float(input("enter the value of y3:"))
+# side1=((x2-x1)**2+(y2-y1)**2)**.5
+# side2=((x3-x2)**2+(y3-y2)**2)**.5
+# side3=((x1-x3)**2+(y1-y3)**2)**.5
+# s=(side1+side2+side3)/2
+# Area=(s*(s-side1)*(s-side2)*(s-side3))**.5
+# print("The area of the triangle:",Area)
+

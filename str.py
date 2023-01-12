ee21d09a191072c72beb35347203cfdf486a6541
Tushar Shukla 12219829 <115138208+shuklatushar12219829@users.noreply.github.com>
2023-01-12 11:53:19 +0530

    Add files via upload

---
+++ str.py
@@ -0,0 +1,10 @@
+str="welcome"
+print(str[2])
+print(str[-1])
+print(str[2:])
+print(str[:4])
+print(str[-7:-1])
+str1="""hello this is tushar shukla from kanpur
+ ,uttar pradesh,
+ studying in lovely professional university"""
+print(str1)

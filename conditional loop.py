8d56bf537db802570dedd7136c6e0d00f5c7ec52
Tushar Shukla 12219829 <115138208+shuklatushar12219829@users.noreply.github.com>
2023-01-13 23:06:14 +0530

    Add files via upload

---
+++ conditional loop.py
@@ -0,0 +1,186 @@
+# # count=0
+# # while(count<100):
+# #     print("Programming is fun")
+# #     count= count+1
+# #
+# # i=0
+# # n=int(input("enter a number:"))
+# # while(n>i):
+# #     print("tushar shukla")
+# #     i=i+1
+#
+# #
+#
+# #
+# # i=1
+# # n=int(input(" number:"))
+# # while(i<=n):
+# #     print((2*i)**2)
+# #     i=i+1
+#
+# # n=int(input("Enter n:"))
+# # i=1
+# # count=0
+# # while(i<=n):
+# #     if(i%2!=0):
+# #         count=count+1
+# #     i=i+1
+# # print(count)
+# # i=1
+# # b=int(input(""))
+# # n=int(input("table"))
+# # while(i<=b):
+# #     print(f"{n} x {i} = {n*i}")
+# #     i=i+1
+#
+# # n=int(input("Enter a number:"))
+# # i=n
+# # product=1
+# # if(n<0):
+# #     print("Not p0ssible")
+# # elif(n==0):
+# #     print("Factorial of zero one")
+# # else:
+# #     while(i<=1):
+# #         product=product*i
+# #         i=i-1
+# #     print(product)
+#
+# # a=int(input("Enter a number:"))
+# # count=0
+# # while(a!=0):
+# #     count=count+1
+# #     a=a//10
+# # print("Counting of digit",count)
+#
+# # a=int(input("Enter a number:"))
+# # sum=0
+# # while(a!=0):
+# #     num1=a%10
+# #     sum=sum+num1
+# #     a = a // 10
+# # print(sum)
+# # rem=0
+# # rev=0
+# # a=int(input("Enter a number:"))
+# # while(a!=0):
+# #     rem=a%10
+# #     rev=rev*10+rem
+# #     a=a//10
+# # print(rev)
+# # if(rev==a):
+# #     print("palandrom")
+# # else:
+# #     print("Not a palandrom") ---all single digit number is a palandrom
+#
+# # n=123
+# # rem=123%10=3
+# # rev=rem*10+rem
+# # n=123//10
+#
+# #H.W
+# #write a program to verify that whether a number is armstrong number or not
+# # #armstrong number
+# #153=1^3 + 5^3 + 3^3=152
+# # sum=0
+# # num1=int(input("Enter a number:"))
+# # a=num1
+# # num=str(num1)
+# # print(len(num))
+# # while(num1%10!=0):
+# #     dig1=num1%10
+# #     sum=sum+dig1**len(num)
+# #     num1=num1//10
+# # print((sum))
+# # if(a==sum):
+# #     print("Number is armstrong number")
+# # else:
+# #     print("Not a armstrong number")
+#
+# i=100
+# while(i>100):
+#     print("printing message on true")
+# else:
+#     print("printing message on false")
+#
+#
+#
+#
+#
+# #
+# # i=1
+# # while(i<=10):
+# #     if(i==2):
+# #         break
+# #     print(i)
+# #     i=i+1
+# # else:
+# #         print("Printing message on false condition")
+# # i=1
+# # sum=0
+# # num=int(input("num"))
+# # while(i<=num):
+# #     sum=sum+i
+# #     i=i+1
+# # else:
+# #     while(num<=0):
+# #         sum=sum+num
+# #         num=num+1
+# # print(("Sum:",sum))
+#
+# # x=int(input("x:"))
+# # y=int(input("y:"))
+# # if(x==0 or y==0):
+# #     print("value must be non zero")
+# # elif(x>y):
+# #     result=x
+# # else:
+# #     result=y
+# # while(result):
+# #      if(x%result==0 and y%result==0):
+# #          break
+# #      result=result-1
+# # print(result)
+#
+# # FIBONNACI SERIES
+# # num1,num2=0,1
+# # n=int(input("Enter n:"))
+# # i=1
+# # print("Finonacci series")
+# # while(i<=n):
+# #     print(num1,num2)
+# #     res=num1+num2
+# #     num1=num2
+# #     num2=res
+# #     i=i+1
+#
+# #      FOR LOOP
+# # list=[2,1,3,5,7,8,4,]
+# # for i in list:
+# #     print(i)
+# #
+# #
+# # list1=["blue","green","red","yellow"]
+# # for i in list1:
+# #     if(i[0]=="Y"or i[0]=="y"):
+# #         print(i)
+# # # else:
+# # #     list1.append()
+# # #     print(list1)
+#
+# # RANGE IN FOR LOOP
+# for i in range(6):
+#     print(i)
+#
+# for i in range(1,6):
+#     print(i)
+#
+# for i in range(1,6,2):
+#     print(i)
+#
+# n=int(input("table for:"))
+# i=1
+# for i in range(1,11):
+#     print(f"{n} x {i} = {n*i}")
+#
+# n=int(input("number:"))
---
+++ unicode.py
@@ -0,0 +1,6 @@
+import sys
+print(sys.maxunicode)
+print(ord('a'))
+print(chr(65))
+print(ord(' '))
+print(sys.getfilesystemecoding())

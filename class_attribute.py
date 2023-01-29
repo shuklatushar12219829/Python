3ccfaac6c24d2920d9aa5e730819b30c4320c299
Tushar Shukla 12219829 <115138208+shuklatushar12219829@users.noreply.github.com>
2023-01-29 23:14:05 +0530

    Add files via upload

---
+++ class_attribute.py
@@ -0,0 +1,16 @@
+# Write Python code here
+class sampleclass:
+    count = 0  # class attribute
+    def increase(self):
+        sampleclass.count += 1
+        #self.count += 1
+
+# Calling increase() on an object
+s1 = sampleclass()
+s1.increase()
+print(s1.count)
+# Calling increase on one more object
+s2 = sampleclass()
+s2.increase()
+print(s2.count)
+print(sampleclass.count)

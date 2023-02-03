9f0840226690f90fa4abc83dfe2751007de7e000
Tushar Shukla 12219829 <115138208+shuklatushar12219829@users.noreply.github.com>
2023-02-03 23:01:43 +0530

    Add files via upload

---
+++ example3_self.py
@@ -0,0 +1,10 @@
+class Student:
+	x=12
+	def __init__(self, name, age, email):
+		self.name = name
+		self.age = age
+		self.email = email
+Stud_1 = Student('SriRam', 25, 'ram@sch.com') 
+Stud_2 = Student('Lakshman', 28, 'laks@sch.com')
+print('Stud_1 details =', Stud_1.name, Stud_1.age, Stud_1.email,Student.x)
+print('Stud_2 details =', Stud_2.name, Stud_2.age, Stud_2.email,Student.x)

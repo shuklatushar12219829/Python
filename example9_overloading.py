5c3d402d757157bc0879db66c40d4b3b64a20d38
Tushar Shukla 12219829 <115138208+shuklatushar12219829@users.noreply.github.com>
2023-02-10 19:05:46 +0530

    Add files via upload

---
+++ example9_overloading.py
@@ -0,0 +1,32 @@
+# class Greeting:
+#
+# 	def sayHello(self, name = None, wish = None):
+# 		if name is not None and wish is not None:
+# 			print ('Hello' + name + wish)
+# 		elif name is not None and wish is None:
+# 			print ('Hello' + name)
+# 		else:
+# 			print ('Hello')
+# greet = Greeting()
+# # Call the method with zero, one and two parameters
+# greet.sayHello()
+# greet.sayHello('Ram')
+# greet.sayHello('Ram,', 'Good Morning!!!')
+
+#area of rectange, circle square
+# class Area:
+# 	def area(self, l=None , b=None , r=None):
+# 		if l is not None and b is not None and r is None:
+# 			print("area of rectange:",l*b)
+# 		elif l is not None and b is None and r is None:
+# 			print("area of square:",l**2)
+# 		elif l is not None and b is not None and r is not None:
+# 			print("area of circle:",3.14*r**2)
+# 			print("area of square:", l ** 2)
+# 			print("area of rectangle:", l*b)
+# a1=Area()
+# a1.area(int(input()),int(input()))
+# a1.area(int(input()))
+# a1.area(int(input()),int(input(),int(input())))
+
+
